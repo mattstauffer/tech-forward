cfbd77ef0166a846fa93fdd2defcc0ec94677f85
Matt Stauffer <matt@tighten.co>
2017-01-24 15:02:18 -0500

    Pull org list from json

--- source/js/app.js
+++ source/js/app.js
@@ -1,11 +1,30 @@
 window.axios = require('axios');
+window.handlebars = require('handlebars');
 
 const client = axios.create({
     baseURL: "/"
 });
 
+var $container = $('.tiles'),
+    template = handlebars.compile($('#org-template').html());
+
 client.get('js/orgs.json')
     .then(response => {
         // create a box for each
         console.log(response.data);
+
+        response.data.forEach(org => {
+            org.num = getRandomInt(1, 15);
+            org.paddedNum = padToTwo(org.num);
+            $container.append(template(org));
+        });
     });
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function padToTwo(number) {
+    if (number <= 10) { number = ("0" + number).slice(-2); }
+    return number;
+}
